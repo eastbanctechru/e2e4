8f0d7aaca9d9df1bca5336bddcec6ac6df0d1f66
Nikolay Matrosov <nikolay.matrosov@gmail.com>
2016-03-14 15:28:38 +0600

    configure tests environment

---
+++ karma.conf.js
@@ -0,0 +1,46 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = function (config) {
+    config.set({
+        browsers: ['PhantomJS'], //run in Chrome
+        singleRun: true, //just run once by default
+        frameworks: ['mocha'], //use the mocha test framework
+        files: [
+            // all files in "test"
+            'tests/**/*.ts'
+            // each file acts as entry point for the webpack configuration
+        ],
+
+        preprocessors: {
+            // add webpack as preprocessor
+            'tests/**/*.ts': [ 'webpack', 'sourcemap' ]
+        },
+        reporters: [ 'dots' ], //report results in this format
+        webpack: {
+            devtool: 'inline-source-map', //just do inline source maps instead of the default
+            module: {
+                loaders: [
+                    {
+                        test: /.*(?!\.d\.ts)|(\.ts)$/,
+                        loader: 'babel?stage=2&optional[]=es7.decorators&optional[]=es7.classProperties&loose[]=all!ts-loader',
+                        include: [
+                            path.resolve(__dirname, 'src'),
+                            path.resolve(__dirname, 'tests')
+                        ]
+                    }
+                ]
+            },
+            resolve: {
+                modulesDirectories: [
+                    'node_modules'
+                ],
+                extensions: ['', '.ts', '.tsx', '.json', '.js']
+            }
+        },
+        webpackServer: {
+            noLog: true,
+            noInfo: true //please don't spam the console when running in karma!
+        }
+    });
+};
---
+++ tests/test.ts
@@ -0,0 +1,8 @@
+import * as chai from 'chai';
+const { expect } = chai;
+
+describe('config', () => {
+    it('should run', () => {
+        expect(true).eql(true);
+    });
+});
