a51ba0c7faa98c5eb6b77f2107cf53cd4363d435
Nikolay Matrosov <nikolay.matrosov@gmail.com>
2016-03-18 15:01:46 +0600

    wallaby.js

---
+++ wallaby.js
@@ -0,0 +1,40 @@
+var babel = require('babel');
+var wallabyWebpack = require('wallaby-webpack');
+var webpackPostprocessor = wallabyWebpack({});
+
+module.exports = function (wallaby) {
+    return {
+        files: [
+            { pattern: 'src/**/*.ts', load: false },
+            { pattern: 'src/**/*.d.ts', ignore: true }
+        ],
+
+        tests: [
+            { pattern: 'tests/**/*.ts', load: false }
+        ],
+
+        preprocessors: {
+            '**/*.js*': function(file) { return babel.transform(file.content, {stage: 2, loose: "all",
+                optional: [
+                    "es7.decorators",
+                    "es7.classProperties"
+                ]}) }
+        },
+
+        postprocessor: webpackPostprocessor,
+        env: {
+            type: 'browser',
+            runner: require('phantomjs-prebuilt').path,
+            params: {
+                runner: '--web-security=false'
+            }
+        },
+
+        testFramework: 'mocha@2.2.4',
+
+        bootstrap: function () {
+            window.__moduleBundler.loadTests();
+        },
+        debug: true
+    };
+};
