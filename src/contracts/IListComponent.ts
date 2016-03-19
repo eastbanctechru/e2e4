e42b5bd6654b5b87a65ac92c8b49f2d1ee0d591c
Fedor Shchudlo <fshchudlo@gmail.com>
2016-03-19 11:26:59 +0600

    onSortChangesCompleted added to list contact

--- src/contracts/IListComponent.ts
+++ src/contracts/IListComponent.ts
@@ -11,4 +11,5 @@ export interface IListComponent extends IComponentWithState, IComponentWithSelec
     clearData(): void;
     reloadData(): void;
     toRequest(): any;
+    onSortChangesCompleted(): void;
 }
