842c39f0ed26f18dda762c464d095580a71b2eda
Fedor Shchudlo <fshchudlo@gmail.com>
2016-05-27 16:56:11 +0600

    SelectionEventsHelper test stub

---
+++ tests/selectionEventsHelper.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import { SelectionEventsHelper } from '../src/selectionEventsHelper';
+describe('SelectionEventsHelper', () => {
+    describe('ctor', () => {
+        it('created with good state', () => {
+            let helper = new SelectionEventsHelper(null);
+            //TODO: write meaningful tests
+
+        });
+    });
+});
