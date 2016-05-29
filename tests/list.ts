4e6106637257a90610811cf50895612a69e06270
Fedor Shchudlo <fshchudlo@gmail.com>
2016-05-29 18:14:58 +0600

    couple of tests

--- tests/list.ts
+++ tests/list.ts
@@ -1,16 +1,51 @@
 import { expect } from 'chai';
 import { List } from '../src/list';
+import { SimplePager } from '../src/simplePager';
+import { ProgressState } from '../src/common/progressState';
 
-class UselessList extends List {
+class SutList extends List {
     getDataReadPromise(): Promise<Object> {
         return null;
     }
 }
+function totarget(): List {
+    return new SutList(new SimplePager());
+}
 
 describe('List', () => {
-    describe('display real coverage', () => {
-        it('not any meaningfull', () => {
-            let list = new UselessList(null);
+    describe('lifecycle', () => {
+        it('init set inited to true', () => {
+            let list = totarget();
+            expect(list.inited).false;
+            list.init();
+            expect(list.inited).true;
+        });
+        it('dispose set disposed to true', () => {
+            let list = totarget();
+            expect(list.disposed).false;
+            list.dispose();
+            expect(list.disposed).true;
+        });
+        it('ready and busy reflects state property', () => {
+            let list = totarget();
+            expect(list.state).eq(ProgressState.Initial);
+            expect(list.ready).true;
+            expect(list.busy).false;
+
+            list.state = ProgressState.Progress;
+            expect(list.ready).false;
+            expect(list.busy).true;
+
+            list.state = ProgressState.Done;
+            expect(list.ready).true;
+            expect(list.busy).false;
+            list.state = ProgressState.Cancelled;
+            expect(list.ready).true;
+            expect(list.busy).false;
+            list.state = ProgressState.Fail;
+            expect(list.ready).true;
+            expect(list.busy).false;
+
         });
     });
 });
