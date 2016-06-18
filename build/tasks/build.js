9dcded44959465b30d0a924e089c033ad2dfa6fa
Fedor Shchudlo <fshchudlo@gmail.com>
2016-06-18 16:24:10 +0600

    index file provided

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -7,32 +7,44 @@ var concat = require('gulp-concat');
 var assign = Object.assign || require('object.assign');
 var merge = require('merge2');
 
-gulp.task('build-es6', function() {
+var getES6options = function () {
     var options = require('../../tsconfig.json').compilerOptions;
     options['target'] = 'es6';
     options['module'] = 'es6';
-    var tsResult = gulp.src(paths.source)
-        .pipe(sourcemaps.init())
-        .pipe(typescript(options));
-    return merge([
-        tsResult.dts.pipe(gulp.dest(paths.esmOutput)),
-        tsResult.js.pipe(sourcemaps.write()).pipe(gulp.dest(paths.esmOutput))
-    ]);
-});
-
-gulp.task('build-commonjs', function() {
+    return options;
+}
+var getCjsOptions = function () {
     var options = require('../../tsconfig.json').compilerOptions;
     options['module'] = 'commonjs';
     options['target'] = 'es5';
-    var tsResult = gulp.src(paths.dtsSrc.concat(paths.source))
-        .pipe(typescript(options));
+    return options;
+}
+
+gulp.task('build-es6', function () {
+    var options = getES6options();
+    var srcResult = gulp.src(paths.source).pipe(sourcemaps.init()).pipe(typescript(options));
+    var indexResult = gulp.src('index.ts').pipe(typescript(options));
     return merge([
-        tsResult.dts.pipe(gulp.dest('src/')),
-        tsResult.js.pipe(gulp.dest('src/'))
+        srcResult.dts.pipe(gulp.dest(paths.esmOutput)),
+        srcResult.js.pipe(sourcemaps.write()).pipe(gulp.dest(paths.esmOutput)),
+        indexResult.dts.pipe(gulp.dest('esm/')),
+        indexResult.js.pipe(gulp.dest('esm/'))
     ]);
 });
 
-gulp.task('build', function(callback) {
+gulp.task('build-commonjs', function () {
+    var options = getCjsOptions();
+    var srcResult = gulp.src(paths.dtsSrc.concat(paths.source)).pipe(typescript(options));
+    var indexResult = gulp.src(paths.dtsSrc.concat('index.ts')).pipe(typescript(options));
+    return merge([
+        srcResult.dts.pipe(gulp.dest('src/')),
+        srcResult.js.pipe(gulp.dest('src/')),
+        indexResult.dts.pipe(gulp.dest('./')),
+        indexResult.js.pipe(gulp.dest('./'))
+    ]);
+});
+
+gulp.task('build', function (callback) {
     return runSequence(
         [
             'build-commonjs',
