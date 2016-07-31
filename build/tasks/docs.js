9dcc4444e42e8397742af2c8b98c196eeff26448
Fedor Shchudlo <fshchudlo@gmail.com>
2016-07-31 14:36:17 +0700

    chore(documentation): tasks to deploy documentation to gh-pages

--- build/tasks/docs.js
+++ build/tasks/docs.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
 var typedoc = require("gulp-typedoc");
+var ghPages = require('gulp-gh-pages');
 var paths = require('../paths');
 
-gulp.task("docs", function () {
+gulp.task("docs-build", function () {
     return gulp
         .src(paths.source)
         .pipe(typedoc({
@@ -11,10 +13,15 @@ gulp.task("docs", function () {
             includeDeclarations: false,
             experimentalDecorators: true,
 
-            out: "./reports/docs",
+            out: paths.docs,
             mode: "file",
             name: "e2e4",
             ignoreCompilerErrors: true,
             version: true
         }));
+});
+
+gulp.task('docs-deploy', function() {
+  return gulp.src('./docs/**/*')
+    .pipe(ghPages());
 });
