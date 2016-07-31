9e3faab215105328230a5dd79a6a1f2aa6e647ab
Fedor Shchudlo <fshchudlo@gmail.com>
2016-07-31 14:45:58 +0700

    refactor(filterAnnotation): replace dafeaultFilterConfig function to filter annotation

--- src/contracts/filter-config.ts
+++ src/contracts/filter-config.ts
@@ -9,17 +9,3 @@
     serializeFormatter?: (value: Object) => Object;
     parseFormatter?: (rawValue: Object, allValues?: Object) => Object;
 }
-
-export function getDefaultFilterConfig(propertyName: string): FilterConfig {
-    return {
-        coerce: true,
-        defaultValue: undefined,
-        emptyIsNull: false,
-        ignoreOnAutoMap: false,
-        parameterName: propertyName,
-        parseFormatter: undefined,
-        persisted: false,
-        propertyName: propertyName,
-        serializeFormatter: undefined
-    } as FilterConfig;
-}
