3382f24601499a52f653c6e2ce54fa12c6115832
Fedor Shchudlo <fshchudlo@gmail.com>
2016-08-08 17:40:35 +0700

    refactor(StatusTracker): refactor to instantiatable class instead of static usage
    BREAKING CHANGE: `StatusTracker` now must be used via creation of specific instances instead of static methods and global `statusList` collection.

--- tests/common/-coverageStub.ts
+++ tests/common/-coverageStub.ts
@@ -1,10 +1,8 @@
 import { expect } from 'chai';
 import { Utility } from '../../src/utility';
-import { StatusTracker } from '../../src/status-tracker';
 
 describe('coverage stub :)', () => {
     it('static ctors', () => {
         expect(new Utility()).not.null;
-        expect(new StatusTracker()).not.null;
     });
 });
