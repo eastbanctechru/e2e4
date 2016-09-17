aa0ccb365f3597918d94ac55422a6ead32958605
Fedor Shchudlo <fshchudlo@gmail.com>
2016-09-17 12:17:52 +0700

    [Prerelease] Bumped version number

--- build/tasks/release.js
+++ build/tasks/release.js
@@ -34,7 +34,6 @@ gulp.task('conventional-release', function (done) {
 
 gulp.task('release', function (callback) {
     runSequence(
-        'docs',
         'git-commit-changes',
         'git-push-changes',
         'git-create-new-tag',
