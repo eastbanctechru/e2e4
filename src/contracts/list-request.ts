e13be1ce97dfd18e541d618aab0ea4e9de372439
Fedor Shchudlo <fshchudlo@gmail.com>
2016-10-03 11:11:09 +0700

    feat(paging): separate `PagedListRequest` contract added
    BREAKING CHANGE: `take` and `skip` properties of `ListRequest` moved to `PagedListRequest`

--- src/contracts/list-request.ts
+++ src/contracts/list-request.ts
@@ -9,12 +9,4 @@ export interface ListRequest {
      * Sortings, which must be applied to the data on next request.
      */
     sortings: Array<SortParameter>;
-    /**
-     * How many items already loaded to the list and must be skipped on next request.
-     */
-    skip?: number;
-    /**
-     * How many items must be loaded on next request.
-     */
-    take?: number;
 }
