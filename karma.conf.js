6ac3a47b8f49c324af0bd1ed0a66824e99d3acb9
Fedor Shchudlo <fshchudlo@gmail.com>
2016-12-03 15:00:14 +0700

    chore(all): return phantom back

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 module.exports = function (config) {
     config.set({
-        browsers: ['Chrome'],
+        browsers: ['PhantomJS'],
         singleRun: true,
         frameworks: ['mocha'],
         files: [
@@ -20,12 +20,6 @@ module.exports = function (config) {
             reporters: [
                 { type: 'lcov', subdir: 'coverage' }
             ]
-        },
-        customLaunchers: {
-            Chrome_travis_ci: {
-                base: 'Chrome',
-                flags: ['--no-sandbox']
-            }
         },
         webpack: {
             devtool: 'inline-source-map',
@@ -65,7 +59,4 @@ module.exports = function (config) {
             noInfo: true
         }
     });
-    if (process.env.TRAVIS) {
-        config.browsers = ['Chrome_travis_ci'];
-    }
 };
